401d9a8e9f0c194a9bdf8290373c566e256c58f9
Tr1dae <121827564+Tr1dae@users.noreply.github.com>
2025-02-16 13:18:19 -0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 echo Installing dependencies globally...
 pip install --upgrade pip
 pip install opencv-python PyQt6 ffmpeg-python numpy
-echo Installation complete. You can now run the app using run_without_venv.bat.
+echo Installation complete. You can now run the app using run.bat.
 pause
